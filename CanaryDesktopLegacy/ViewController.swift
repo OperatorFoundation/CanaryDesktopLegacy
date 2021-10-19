9863f0cca495a2b960a10a9a801c0359913228cc
consuelita <adelita@operatorfoundation.org>
2021-10-19 17:34:32 -0500

    Initial Commit

---
+++ CanaryDesktopLegacy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CanaryDesktopLegacy
+//
+//  Created by Mafalda on 10/19/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
